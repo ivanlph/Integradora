1605ad5588bf5a498ea996ff44cce8d202bab3a2
Alex <alex1071756@gmail.com>
2016-12-05 01:56:20 -0800

    Base de datos remota

--- letrelon/vistas/productos.php
+++ letrelon/vistas/productos.php
@@ -7,13 +7,13 @@
 <div class="row">
   <?php 
 
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
+    $servername = "sql9.freemysqlhosting.net";
+    $username = "sql9147877";
+    $password = "a89gR3dXT1";
 
 
     try {
-      $conn =  new PDO("mysql:host=$servername;dbname=letrelom", $username, $password);
+      $conn =  new PDO("mysql:host=$servername;dbname=sql9147877", $username, $password);
       $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
       $sql = 'SELECT Nombre, Descripcion, Precio, Imagen FROM productos';
