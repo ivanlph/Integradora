434389ee3e28b2ac35e6c937e5928f8b21ef5c6e
pegatuga <kyarysix@gmail.com>
2016-12-11 15:39:20 -0800

    nyu

--- letrelon/vistas/nosotros.php
+++ letrelon/vistas/nosotros.php
@@ -1,44 +1,35 @@
-
-<html>
-<body>
-<form class = "form" enctype="multipart/form-data" role = "form" method = "post" action = "vistas/nosotros.php">
 <div class="container">
-  
-<nav>
-  <ul>
-   <img src="integradora/recursos/images/logo.jpg" alt="" style="width:304px;height:228px;">
-    <a href="http://localhost:8080/integradora/letrelon/index.php?ver=inicio"> inicio</a>
-  </ul>
-</nav>
-
+  <center>
+   <img src="./recursos/images/logo1.png" class ="img-responsive"> 
+  </center>
+  <div class="jumbotron" style="background-color:#333;">
 <article>
-  <h1>Letreron</h1>
-  <p>Letreron es una empresa dedicada a dar los servicios de instalaci&oacute;n de mallasombras como su especialidad.</p>
-  <p>Con la ventaja de que tambi&eacute;n es una imprenta, hacen camisetas a tu preferencia,tazas,calcomanias, brinca brinca y muchas cosas m&aacute;s &#9829;.</p>
-</article>
-
-    <aside role="complementary">
-
+  <h1>Letrelon</h1>
+  <p><font color="white" size=4>Letrelon es una empresa dedicada a dar los servicios de instalaci&oacute;n de mallasombras como su especialidad.</font></p>
+  <p><font color="white" size=4> Con la ventaja de que tambi&eacute;n es una imprenta, hacen camisetas a tu preferencia,tazas,calcomanias, brinca brinca y muchas cosas m&aacute;s &#9829;.</font></p>
+</div>
+<div class="jumbotron" style="background-color:#333;">
         <div class="col" id="col3">
-          <h6>OBJETIVO</h6><br />
+          <h2>OBJETIVO</h2><br />
             <br />
-            aqui va el objetivo<br />
-            <br />
-            <br />
-            <h6>MISION</h6><br />
-            <br />
-            Distribuir los productos y servicios de la mejor calidad al mejor precio<br />
+            <font color="white" size=4>aqui va el objetivo</font><br />
             <br />
             <br />
-            <h6>VISION</h6><br />
+            <h2>MISION</h2><br />
             <br />
-            Se visualiza que la empresa se estabilice en muchas partes de la república Mexicana, con los mejores materiales, precios y mano de obra para hacer de tus necesidades nuestra arte <br />
+            <font color="white" size=4>Distribuir los productos y servicios de la mejor calidad al mejor precio</font><br />
+            <br />
+            <br />
+            <h2>VISION</h2><br />
+            <br />
+            <font color="white" size=4> Se visualiza que la empresa se estabilice en muchas partes de la república Mexicana, con los mejores materiales, precios y mano de obra para hacer de tus necesidades nuestra arte</font><br />
             <br />
             <br />
           </div>
-          <!--este es el mapa-->
-          <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m11!1m3!1d239.82898813008796!2d-114.78822776649783!3d32.44940008016894!2m2!1f124.07081211360268!2f41.69361367845907!3m2!1i1024!2i768!4f20!3m3!1m2!1s0x0%3A0x5ce916e95ef8ab34!2sLetreros+Y+Lonas!5e1!3m2!1ses-419!2sus!4v1481488191941" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 </div>
-</body>
-</html>
-</from>
+</article>
+<div class="jumbotron" style="background-color:#333;">
+  <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m11!1m3!1d239.82898813008796!2d-114.78822776649783!3d32.44940008016894!2m2!1f124.07081211360268!2f41.69361367845907!3m2!1i1024!2i768!4f20!3m3!1m2!1s0x0%3A0x5ce916e95ef8ab34!2sLetreros+Y+Lonas!5e1!3m2!1ses-419!2sus!4v1481488191941" width="800" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
+          
+</div>
+</div>
