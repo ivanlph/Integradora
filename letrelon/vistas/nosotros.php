ffdcc06109bbc645057f3cb07801cc10e11ebc89
pegatuga <kyarysix@gmail.com>
2016-12-11 12:32:25 -0800

    mapa

--- letrelon/vistas/nosotros.php
+++ letrelon/vistas/nosotros.php
@@ -36,6 +36,8 @@
             <br />
             <br />
           </div>
+          <!--este es el mapa-->
+          <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m11!1m3!1d239.82898813008796!2d-114.78822776649783!3d32.44940008016894!2m2!1f124.07081211360268!2f41.69361367845907!3m2!1i1024!2i768!4f20!3m3!1m2!1s0x0%3A0x5ce916e95ef8ab34!2sLetreros+Y+Lonas!5e1!3m2!1ses-419!2sus!4v1481488191941" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 </div>
 </body>
 </html>
